e789a9a5f18bcff692a5eb7b4dfe482199407dcd
unknown <a.roberto08@gmail.com>
2019-05-27 17:35:11 +0100

    Criado o esqueleto da classe ApostaEuromilhoesTest para implementar testes JUnit

---
+++ src/antrob/santacasatest/ApostaEuromilhoesTest.java
@@ -0,0 +1,39 @@
+package antrob.santacasatest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApostaEuromilhoesTest {
+
+	@Test
+	void testApostaEuromilhoes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testApostaEuromilhoesIntArrayIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNumeros() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEstrelas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAcertos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
